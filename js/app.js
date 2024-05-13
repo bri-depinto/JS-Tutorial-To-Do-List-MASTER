34a6f1879afbea37fa52f6ce459ccfff44b77358
bri-depinto <briannadepinto@gmail.com>
2024-05-13 10:28:48 -0400

    first part of java

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 // CODE EXPLAINED channel
+
+//Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//show todays date
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US," options);
