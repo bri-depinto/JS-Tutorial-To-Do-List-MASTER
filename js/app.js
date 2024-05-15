e0836b217b4bc4530f784c703c760016287e7ddc
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-05-15 13:03:59 +0000

    more java

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
+// variables
+let LIST = [],
+ id = 0;
+
 //show todays date
 const options = {weekday : "long", month:"short", day:"numeric"};
 const today = new Date();
@@ -19,15 +23,59 @@ dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 // add to do function
 
-function addToDo(toDo){
+function addToDo(toDo, id, done, trash){
+
+if(trash){return; }
+const DONE = done? CHECK : UNCHECK;
+const LINE = done? LINE_THROUGH : "";
 
     const item = `<li class="item">
-                    <i class="fa fa-circle-thin co" job="complete" id="0></i>
-                    <p class="text">${toDo}</p>
-                    <i class= "fa fa-trash-o de" job="delete" id="0"> </i>
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}
+                    ">${toDo}</p>
+                    <i class= "fa fa-trash-o de" job="delete" id="${id}"> </i>
                 </li>
                 `;
         const position = "beforeend";
         list.insertAdjacentHTML(position, item);
 }
-addToDo("drink coffe");
+//add an item to the list user the enter key
+document.addEventListener("keyup", function(even){
+if(event.keyCode === 13){
+    const toDo = input.value;
+
+    //if the iunput isnt empty
+    if(toDo){
+        addToDo(toDo, id, false, false);
+
+        LIST.push({
+            name : toDo,
+            id : id,
+            done : false,
+            trash : false
+        });
+
+        id++;
+    }
+    input.value = "";
+}
+});
+
+//complete to do
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+//target the items created dynamically
+
+list.addEventListener("click", function(event){
+    const element = event.target; //return the licked elemtn inside list
+    const elementJob = element.attributes.job.value; // complete or delete
+
+    if(elementJob == "complete"){
+        completeToDo(element);
+    }else if(elementJob == "delete"){
+        removeToDo(element);
+    }
+});
